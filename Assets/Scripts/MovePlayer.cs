fdb3f59512669c3d743b1e8fe61168fb4634fc83
mrzamay <timofeev.ivaan@gmail.com>
2025-06-16 02:13:50 +0300

    Добавлена игра

---
+++ Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public float HorizontalInput;
+    public float VerticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+
+
+        transform.Translate(Vector3.right * Time.deltaTime * speed * HorizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * speed * VerticalInput);
+
+        void OnColliderEnter2D(Collider2D other)
+        {
+            Debug.Log("hsflkjsahkjfhdlkjfds");
+        }
+    }
+}
